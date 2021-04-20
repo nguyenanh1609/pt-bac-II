4fa2b69d7ca0547b763a2a14de568d28f8ceb7dd
nguyenanh1609 <nguyenhuuanh102000@gmail.com>
2021-04-20 18:57:31 +0700

    done pt

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ function kiemTraPhuongTrinhBac2() {
     let b = document.getElementById("b").value
     let c = document.getElementById("c").value
 
-
-    if (a == 0) {
+    if (isNaN(a) || isNaN(b) || isNaN(c)) {
+        document.getElementById("kq").innerText = "not a number value"
+    }
+    else if (a == 0) {
         if (b == 0) {
             if (c == 0) {
                 document.getElementById("kq").innerText = "Phuong trinh vo so nghiem"
@@ -15,20 +17,60 @@ function kiemTraPhuongTrinhBac2() {
             }
         }
         else {
+            document.getElementById("pt").innerText = a + "x^2" + b + "x" + c + "=0"
             document.getElementById("kq").innerText = "Phuong trinh co nghiem duy nhat (-c/b):" + (-c / b)
 
         }
     }
     else {
-        var delta = b*b - 4*a*c;
+        var delta = b * b - 4 * a * c;
         if (delta < 0) {
+            if (b < 0 && c < 0) {
+                document.getElementById("pt").innerText = a + "x^2" + b + "x" + c + "=0"
+            }
+            else if (b > 0 && c > 0) {
+                document.getElementById("pt").innerText = a + "x^2" + "+" + b + "x" + "+" + c + "=0"
+
+            }
+            else if (b < 0 && c > 0) {
+                document.getElementById("pt").innerText = a + "x^2" + b + "x" + "+" + c + "=0"
+            }
+            else if (b > 0 && c < 0) {
+                document.getElementById("pt").innerText = a + "x^2" + "+" + b + "x" + c + "=0"
+            }
             document.getElementById("kq").innerText = "vô nghiệm"
         }
         else if (delta == 0) {
+            if (b < 0 && c < 0) {
+                document.getElementById("pt").innerText = a + "x^2" + b + "x" + c + "=0"
+            }
+            else if (b > 0 && c > 0) {
+                document.getElementById("pt").innerText = a + "x^2" + "+" + b + "x" + "+" + c + "=0"
+
+            }
+            else if (b < 0 && c > 0) {
+                document.getElementById("pt").innerText = a + "x^2" + b + "x" + "+" + c + "=0"
+            }
+            else if (b > 0 && c < 0) {
+                document.getElementById("pt").innerText = a + "x^2" + "+" + b + "x" + c + "=0"
+            }
             document.getElementById("kq").innerText = "có nghiệm kép" + ":" + -b / (2 * a);
 
         }
         else {
+            if (b < 0 && c < 0) {
+                document.getElementById("pt").innerText = a + "x^2" + b + "x" + c + "=0"
+            }
+            else if (b > 0 && c > 0) {
+                document.getElementById("pt").innerText = a + "x^2" + "+" + b + "x" + "+" + c + "=0"
+
+            }
+            else if (b < 0 && c > 0) {
+                document.getElementById("pt").innerText = a + "x^2" + b + "x" + "+" + c + "=0"
+            }
+            else if (b > 0 && c < 0) {
+                document.getElementById("pt").innerText = a + "x^2" + "+" + b + "x" + c + "=0"
+            }
             document.getElementById("kq").innerText = "có 2 nghiệm" + ":" + (-b + Math.sqrt(delta)) / (2 * a) + " " + "và" + " " + (-b - Math.sqrt(delta)) / (2 * a)
 
         }
